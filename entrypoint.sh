2fb08a3e9ee4a55758523f7f93d6df499712f3cd
junyakaneko <junya@mpsamurai.org>
2021-04-29 14:40:06 +0900

    Quate command inputs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python /code/card.py --list-id=$1 --name=$2 --key=$TRELLO_KEY --token=$TRELLO_TOKEN
+python /code/card.py --list-id='$1' --name='$2' --key='$TRELLO_KEY' --token='$TRELLO_TOKEN'
